6eeacb53a356004b3c81c93770b660bd282e9b45
adaxiang <adaxiang3104@gmail.com>
2024-07-21 12:12:59 +0800

    remove unused

--- app/main.py
+++ app/main.py
@@ -5,11 +5,6 @@ from app.core.cambridge import CambridgeDictScraper
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.get("/word/{word}")
 def read_item(word: str):
     return CambridgeDictScraper().get_ipa(word)
