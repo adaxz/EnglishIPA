bfe58fa5fdcaa8c3ad604d67229edb1a9d21514b
adaxiang <adaxiang3104@gmail.com>
2024-07-21 17:06:15 +0800

    add root

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from app.core.cambridge import CambridgeDictScraper
 app = FastAPI()
 
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
 @app.get("/word/{word}")
 def read_item(word: str):
     return CambridgeDictScraper().get_ipa(word)
